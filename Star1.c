03ebc25cca02021e5e1f7042b04ab4cfc5df8f62
yashodip250303 <yashodipjadhav006@gmail.com>
2026-07-09 15:26:18 +0530

    Create Star1.c

---
+++ Star1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int i,j,k;
+    for(i=0;i<5;i++){
+        for(k=0 ;k<5-i;k++){
+            
+        }
+        
+        for(j=0 ;j<5-i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
