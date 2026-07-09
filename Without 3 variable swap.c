d288a50866ab12ea7b303bb2977aebadbaa72622
yashodip250303 <yashodipjadhav006@gmail.com>
2026-07-09 15:28:47 +0530

    Add integer swap logic without third variable
    Implement a method to swap two integers without using a third variable.

---
+++ Without 3 variable swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a,b;
+      printf("Enter Number A: ");
+      scanf("%d",&a);
+      printf("Enter Number B: ");
+      scanf("%d",&b);
+    
+    a=a+b; 
+    b=a-b;
+    a=a-b;
+
+    
